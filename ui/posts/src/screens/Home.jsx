675d87da68aa71e3e1cfc340ee4130cca69dea5e
ayesha706 <ayesha@itbeam.com>
2024-08-15 18:19:43 +0500

    corrections

--- ui/posts/src/screens/Home.jsx
+++ ui/posts/src/screens/Home.jsx
@@ -3,14 +3,16 @@ import { Comments } from '../sections/Comments'
 export const Home = () => {
     return (
         <div>
-            <div className="flex flex-col justify-center items-center py-7 bg-gray-100">
+            <div className="flex flex-col justify-center items-center py-7">
                 <div className="bg-white rounded-lg shadow-lg overflow-hidden max-w-lg w-full py-4">
-                    <div className="flex justify-between items-center">
                         <div className=" mb-1 p-3">
-                            <span className="text-gray-800 font-semibold">John Doe</span><br />
-                            <span className="text-gray-600  ">2 hours ago</span>
+                            <div className='flex justify-between'>
+                            <span className="text-gray-800 font-semibold">John Doe</span>
+                            <button> +Follow</button>
+                            </div>
+                          
+                            <span className="text-gray-600 text-sm ">2 hours ago</span>
                         </div>
-                    </div>
                     <div className="p-3">
                         <p className="text-gray-700 leading-tight mb-4">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu sapien porttitor, blandit velit ac,
--- ui/posts/src/sections/CreatePost.jsx
+++ ui/posts/src/sections/CreatePost.jsx
@@ -4,19 +4,16 @@ export const CreatePost = () => {
     return (
         <div>
             <div className="heading text-center font-bold text-2xl m-5 text-gray-800"> Create New Post</div>
-            <div className="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
-                <input className="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" spellcheck="false" placeholder="Title" type="text" />
-                <textarea className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellcheck="false" placeholder="Describe everything about this post here"></textarea>
+            <div className="editor mx-auto w-8/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
+                <input className="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" placeholder="Title" type="text" />
+                <textarea className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" placeholder="Describe everything about this post here"></textarea>
 
-                <div className="icons flex text-gray-500 m-2">
+                <div className="icons flex justify-between text-gray-500 m-2">
                  <input type='file'/>
                   
+                 <div className="btn border border-indigo-500 p-1 px-4 float-end font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">Post</div>
                 </div>
 
-                <div className="buttons flex">
-                    <div className="btn border border-gray-300 p-1 px-4 font-semibold cursor-pointer text-gray-500 ml-auto">Cancel</div>
-                    <div className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">Post</div>
-                </div>
             </div>
         </div>
     )
