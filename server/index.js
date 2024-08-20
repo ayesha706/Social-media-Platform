c6f611b03308d03f8ffc68aa2c59e9e0691fc348
ayesha706 <ayesha@itbeam.com>
2024-08-20 17:20:47 +0500

    socket.io implementation

--- server/index.js
+++ server/index.js
@@ -18,26 +18,38 @@ app.use('/api', authRoutes);
 app.use('/api/posts', postRoutes);
 
 const port = process.env.PORT || 8000
-const wsServerPort = 8081;
-const wsServer = new WebSocket.Server({ port: wsServerPort });
+//WebSockets
+// const wsServerPort = 8081;
+// const wsServer = new WebSocket.Server({ port: wsServerPort });
 
-wsServer.on('connection', (socket) => {
-  console.log('Client connected');
+// wsServer.on('connection', (socket) => {
+//   console.log('Client connected');
 
-  socket.on('message', (message) => {
-    console.log('Received:', message);
-    socket.send('Message received: ' + message);
+//   socket.on('message', (message) => {
+//     console.log('Received:', message);
+//     socket.send('Message received: ' + message);
+//   });
+
+//   socket.on('close', () => {
+//     console.log('Client disconnected');
+//   });
+
+//   socket.on('error', (error) => {
+//     console.error('WebSocket error:', error);
+//   });
+// });
+
+// console.log(`WebSocket server is running on ws://localhost:${wsServerPort}`);
+//socket.io
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
   });
-
-  socket.on('close', () => {
-    console.log('Client disconnected');
-  });
-
-  socket.on('error', (error) => {
-    console.error('WebSocket error:', error);
-  });
-});
-
+})
 
 connectDB().then((res) => {
     app.listen(port , () => { console.log(`Server started in port ${port}`) })
--- ui/posts/src/screens/Msg.jsx
+++ ui/posts/src/screens/Msg.jsx
@@ -1,63 +1,80 @@
 import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+const socket = io('http://localhost:9000');
 
 export const WebSocketClient = () => {
   const [ws, setWs] = useState(null);
   const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState('');
+// WebSockets
+  // useEffect(() => {
+  //   const socket = new WebSocket('ws://localhost:8081');
 
+  //   socket.onopen = () => {
+  //     console.log('Connected to the server');
+  //   };
+
+  //   socket.onmessage = (event) => {
+  //     const message = event.data;
+  //     setMessages(prevMessages => [...prevMessages, message]);
+
+  //     if (Notification.permission === 'granted') {
+  //       new Notification('New message', {
+  //         body: message,
+  //       });
+  //     }
+  //   };
+
+  //   socket.onclose = () => {
+  //     console.log('Disconnected from the server');
+  //   };
+
+  //   setWs(socket);
+
+  //   if (Notification.permission !== 'granted') {
+  //     Notification.requestPermission();
+  //   }
+  //   return () => {
+  //     socket.close();
+  //   };
+
+  // }, []);
+  // const sendMessage = () => {
+  //   if (ws && inputValue) {
+  //     ws.send(inputValue);
+  //     setInputValue('');
+  //   }
+  // };
+  //Socket.io
   useEffect(() => {
-    const socket = new WebSocket('ws://localhost:8081');
-
-    socket.onopen = () => {
-      console.log('Connected to the server');
+    socket.on('message', (message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+    return () => {
+      socket.off('message');
     };
-
-    socket.onmessage = (event) => {
-      const message = event.data;
-      setMessages(prevMessages => [...prevMessages, message]);
-
-      if (Notification.permission === 'granted') {
-        new Notification('New message', {
-          body: message,
-        });
-      }
-    };
-
-    socket.onclose = () => {
-      console.log('Disconnected from the server');
-    };
-
-    setWs(socket);
-
-    if (Notification.permission !== 'granted') {
-      Notification.requestPermission();
-    }
-
-  
   }, []);
-
-  const sendMessage = () => {
-    if (ws && inputValue) {
-      ws.send(inputValue);
-      setInputValue(''); 
-    }
+  const handleSendMessage = () => {
+    socket.emit('user-message', inputValue);
+    setInputValue('');
   };
 
   return (
     <div>
-      <div>
-        <input
-          type="text"
-          value={messages}
-          onChange={(e) => setMessages(e.target.value)}
-        />
-        <button onClick={() => sendMessage('user2')}>Send to User 2</button>
-      </div>
-      <div>       
-        {messages.map((msg, index) => (
-          <div key={index}>{msg}</div>
-        ))}
-      </div>
+    <input
+      type="text"
+      id="messageInput"
+      placeholder="Type a message..."
+      value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)}
+    />
+    {/* <button onClick={sendMessage}>Send</button> */}
+    <button onClick={handleSendMessage}>Send</button>
+    <div id="messages">
+      {messages.map((message, index) => (
+        <div key={index}>{message}</div>
+      ))}
     </div>
+  </div>
   );
 };
